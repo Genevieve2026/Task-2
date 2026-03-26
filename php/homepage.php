e3399e0bde50b4ed496748d4c7bbc8cd15584230
Genevieve2026 <genevieve.lahai@uhsw.com>
2026-03-26 11:34:47 +0000

    homepage changes

---
+++ php/homepage.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GLH Homepage</title>
+    <link rel="stylesheet" href="../css/homepage.css">
+    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><img src="../pictures/GLH logo.png" alt="GLH Logo" class="logo"></li>
+                <li><a href="marketplace.php">Marketplace</a></li>
+                <li><a href="categories.php">Categories</a></li>
+                <li><a href="delivery&collection.php">Delivery and <br>Collection</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="../php/index.php">Sign in</a></li>
+                <li><button><a href="../php/index.php">Join Us Today</a></button></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section class="hero">
+            <h1>Welcome to Greenfield Local Hub</h1>
+            <h1>Find local farmers and producers near you</h1>
+            <form>
+                <div class="search">
+                    <i class="fas fa-magnifying-glass"></i>
+                    <input type="text" placeholder="What are you looking for? Seasonal items...? Locations...?" name="search">
+                </div>
+            </form>
+
+            <p>At GLH, we are committed to improving and expanding sustainability efforts in our community. Our team of dedicated professionals is here to support you every step of the way.</p>
+            
+        </section>
+
+        <section class="card-container intro">
+            <h1>Greenfield Local Hub</h1>
+            <p>is chosen time and time again for its commitment to sustainability and community<br> 
+                engagement as well as caring for our local farmers and producers .</p>
+        </section>
+
+        <section class="card-container">
+            <div class="card">
+                <img src="" alt="community">
+                <h2>Community Engagement</h2>
+                <p>We actively engage with our local community through events, workshops, and partnerships with local organizations. We believe in fostering a sense of community and supporting local initiatives that promote sustainability.</p>
+            </div>
+            
+            <div class="card">
+                <img src="" alt="community">
+                <h2>Community Engagement</h2>
+                <p>We actively engage with our local community through events, workshops, and partnerships with local organizations. We believe in fostering a sense of community and supporting local initiatives that promote sustainability.</p>
+            </div>
+
+            <div class="card">
+                <img src="" alt="community">
+                <h2>Community Engagement</h2>
+                <p>We actively engage with our local community through events, workshops, and partnerships with local organizations. We believe in fostering a sense of community and supporting local initiatives that promote sustainability.</p>
+            </div>
+
+            <div class="card">
+                <img src="" alt="community">
+                <h2>Community Engagement</h2>
+                <p>We actively engage with our local community through events, workshops, and partnerships with local organizations. We believe in fostering a sense of community and supporting local initiatives that promote sustainability.</p>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+
+            <img src="../pictures/GLH logo.png" alt="GLH Logo" class="logo">
+            <h2>Sustainable as one, Sustainable together.</h2>
+
+            <div class="contact-info">
+                <h3>Contact Us:</h3>
+                <h3>Email:</h3>
+                <p>GreenfieldLocalHub@gmail.com</p>
+                <h3>Phone:</h3>
+                <p>123-456-7890</p>
+                <h3>Address:</h3>
+                <p> 82 Downes Street, London, SW10 7BZ</p>
+            </div>
+            <div class="social-media">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="quick-links">
+                <h3>Quick Links:</h3>
+                <ul>
+                    <li><a href="marketplace.php">Marketplace</a></li>
+                    <li><a href="categories.php">Categories</a></li>
+                    <li><a href="delivery&collection.php">Delivery and Collection</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="../php/index.php">Sign in</a></li>
+                    <li><a href="../php/index.php">Join Us Today</a></li>
+                    <li><a href="../php/GLHLoyalty.php">GLHLoyalty</a></li>
+                </ul>
+            </div>
+            <div class="newsletter">
+                <p>Subscribe to our newsletter:</p>
+                <form>
+                    <input type="email" placeholder="Enter your email" name="email">
+                    <button type="submit">Subscribe</button>
+                </form>
+            </div>
+            <div class="banks">
+                <p>We accept:</p>
+                <img src="../pictures/VISA.png" alt="Visa">
+                <img src="../pictures/mastercard.png" alt="MasterCard">
+                <img src="../pictures/AMEX.png" alt="American Express">
+            </div>
+            <div class="legal">
+                <p><a href="#">Copyright Notices</a> | <a href="#">Terms of Service</a></p>
+                <p><a href="#">Terms and Conditions</a> | <a href="#">Food Safety and Hygiene Regulations</a></p>
+                <p><a href="#">Privacy Policy</a> | <a href="#">Return Policy</a></p>
+
+        <p>&copy; 2026 Greenfield Local Hub. All rights reserved.</p>
+    </footer>
+    
+</body>
+</html>
